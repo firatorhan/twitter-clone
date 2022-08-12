2c53a937038962fdf7f91cff322e53a6ede194c4
Fırat ORHAN <forhan@innova.com.tr>
2022-08-12 17:12:08 +0300

    add content area and remove firebase connection

--- src/layout/Content.js
+++ src/layout/Content.js
@@ -1,18 +1,33 @@
-import React, {useEffect} from 'react'
+import React from "react";
+import Divider from "../components/Divider";
+import FeedList from "../components/FeedList";
+import TweetBox from "../components/TweetBox";
 
+import { PopulerIcon } from "../icons/Icons";
 
 const Content = () => {
-
-  useEffect(() => {
   
-  })
-
+  
 
   return (
-    <main className='bg-purple-200 flex-1'>
-        <h1>Content</h1>
-    </main>
-  )
-}
+    <main className="flex-1 flex flex-col border-r border-l border-gray-extraLight">
+      <header className="sticky top-0 z-10 bg-white flex justify-between items-center p-4 border-b border-gray-extraLight ">
+        <span className="font-bold text-xl text-gray-900">Home</span>
+        <PopulerIcon className="w-6 h-6 text-primary-base" />
+      </header>
+      <div className="flex space-x-4 px-4 py-3">
+        <img
+          src="https://cdn-icons.flaticon.com/png/512/3899/premium/3899618.png?token=exp=1660223355~hmac=cdb4f6412161ffbe4ebf4b120009fbbd"
+          alt="Profile"
+          className="w-11 h-11 rounded-full"
+        />
+        <TweetBox />
+      </div>
+      <Divider />
 
-export default Content
+      {/* Feed */}
+      <FeedList />
+    </main>
+  );
+};
+export default Content;
